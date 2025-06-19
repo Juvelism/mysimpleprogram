5ad1211d408d2fe44d4537b5bbb091199a1ea65c
Juvs <jborrel01@gmail.com>
2025-06-19 20:35:35 +0800

    updates

--- ticket.py
+++ ticket.py
@@ -1,28 +1,35 @@
 # TODO: TICKETING
 
-print("Welcome to the rollercoaster!")
-height = int(input("What is your height in cm? "))
-bill = 0
+def customer_height(h):
 
-if height >= 120:
-    print("You can ride the rollercoaster!")
-    age = int(input("What is your age? "))
-    if age < 12:
-        bill = 5
-        print("Child tickets are $5.")
-    elif age <= 18:
-        bill = 7
-        print("Youth tickets are $7.")
+    if h >= 120:
+
+        return True
+
     else:
-        bill = 12
-        print("Adult tickets are $12.")
+
+        print("You are too short for the rides.")
+        return False
+
+def customer_age(a):
+    bill = 0
+    if a <= 12:
+        bill += 5
+
+    return bill
+
+def main():
+    choice = int(input("Please enter your height: "))
+
+    if not customer_height(choice):
+        return
+
+    age = int(input("Enter your age: "))
+
+    print(f"Your total price: {customer_age(age)}")
+    print(f"Enjoy the rides.")
+
+main()
 
 
-    wants_photo = input("Do you want a photo taken? Y or N. ")
-    if wants_photo == "Y":
-        bill += 3
-
-    print(f"Your final bill is ${bill}")
-
-else:
-    print("Sorry, you have to grow taller before you can ride.")
+main()
