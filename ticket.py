a32d06a7f7f68f5db602db79ca2e0bfc662043dd
Juvs <jborrel01@gmail.com>
2025-06-20 14:04:36 +0800

    updates

--- ticket.py
+++ ticket.py
@@ -15,6 +15,20 @@ def customer_age(a):
     bill = 0
     if a <= 12:
         bill += 5
+        print(f"You are young")
+    elif a <= 18:
+        bill += 7
+        print(f"You are adult")
+    elif a < 45:
+        bill += 7
+        print(f"You are big adult")
+
+    elif 45 >= a <= 55:
+        print("Free of charge")
+
+    else:
+        bill += 12
+        print(f"Your are super adult")
 
     return bill
 
