737b5c40e8f2fa39d1e1dcffdfd5706710517e3b
Juvs <jborrel01@gmail.com>
2025-06-22 17:47:39 +0800

    updates

--- pass_generator.py
+++ pass_generator.py
@@ -10,12 +10,25 @@ small_alphabet = ["a","b","c","d","e",
 
 nums = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]
 
-stock_pw = []
+stocked_pw = []
+
+new_lst = []
 
 a_pw = int(input("How many alphabet characters: "))
+if a_pw < 0:
+    print("Invalid input!")
+
 n_pw = int(input("How many numbers: "))
 
-stock_pw += random.choices(small_alphabet, k=a_pw) + random.choices(nums, k=n_pw)
-random.shuffle(stock_pw)
-final_tip = ''.join(stock_pw)
-print(f"Generated password: {final_tip}")
+stocked_pw += random.choices(small_alphabet, k=a_pw) + random.choices(nums, k=n_pw)
+
+for h in stocked_pw:
+    new_lst.append(h)
+
+generated_pw = ""
+for f in new_lst:
+    generated_pw += f
+
+print(f"Generated Password: {generated_pw}")
+
+
