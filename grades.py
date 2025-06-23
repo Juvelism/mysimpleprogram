f5a1285c2b8edeabe612e241495121b0efc097f7
Juvs <jborrel01@gmail.com>
2025-06-23 11:29:25 +0800

    updates

---
+++ grades.py
@@ -0,0 +1,20 @@
+# Calculating Grade
+
+def name_of_student():
+    name_student = input("Full name: ")
+    return name_student
+
+def grade_of_student():
+    grade_1 = int(input("Enter grade #1: "))
+    grade_2 = int(input("Enter grade #2: "))
+    grade_3 = int(input("Enter grade #3: "))
+    return grade_1, grade_2, grade_3
+
+def calculation_of_grade(g1,g2,g3):
+    sum_g = g1 + g2 + g3
+
+
+def main():
+    pass
+
+main()
