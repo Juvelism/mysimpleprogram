63eb2bdc83fda612e74c22bcf1aaf59ac2025aac
Juvs <jborrel01@gmail.com>
2025-06-23 14:12:30 +0800

    updates

---
+++ age_category_checker.py
@@ -0,0 +1,26 @@
+from errno import ECHILD
+
+
+def get_user_info():
+    user_name = input("Full name: ").title()
+    user_age = int(input("Age: "))
+    return user_name, user_age
+
+def check_category(age):
+    if 0 == age <= 12:
+        return 'Child'
+    elif 13 >= age <= 19:
+        return 'Teenager'
+    elif 20 >- age <= 59:
+        return 'Adult'
+    else:
+        return 'Senior Citizen'
+
+def print_category(name, category):
+    print(f"{name}, you are a { category}")
+
+def main():
+    name, age = get_user_info()
+    print_category(name, check_category(age))
+
+main()
