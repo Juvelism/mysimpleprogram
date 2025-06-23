d162053d3e3ee6b352ebf8e00f1ccf5d5bb0ba6b
Juvs <jborrel01@gmail.com>
2025-06-23 11:41:30 +0800

    updates

--- grades.py
+++ grades.py
@@ -10,11 +10,16 @@ def grade_of_student():
     grade_3 = int(input("Enter grade #3: "))
     return grade_1, grade_2, grade_3
 
-def calculation_of_grade(g1,g2,g3):
+def calculation_of_grade(name,g1,g2,g3):
     sum_g = g1 + g2 + g3
-
+    avg_g = sum_g / 3
+    print("-" * 10)
+    print(f"Student name: {name}")
+    print(f"Average: {avg_g:.2f}")
 
 def main():
-    pass
+    s_name = name_of_student()
+    g1,g2,g3 = grade_of_student()
+    calculation_of_grade(s_name,g1,g2,g3)
 
 main()
