63bf3bce4b1bf10bf2140ec2e564898eea317048
Juvs <jborrel01@gmail.com>
2025-06-26 10:36:12 +0800

    updates

--- multi_user_login_system.py
+++ multi_user_login_system.py
@@ -1,36 +1,38 @@
-admin_credentials = {
-    'admin1': 'admin123',
-    'admin2': 'admin456',
-    'admin3': 'admin789'
-}
 
-def get_login_input():
+def check_credentials():
+
+    admin_credentials = {
+        'admin1': 'admin123',
+        'admin2': 'admin456',
+        'admin3': 'admin789'
+    }
+
     user = input("Username: ")
     password = input("Password: ")
 
-    return user, password
-
-def check_credentials(user_name,user_pass):
-
     for i in admin_credentials:
-        if user_name == i and user_pass == admin_credentials[i]:
+        if user == i and password == admin_credentials[i]:
             return True
         else:
             return False
     return None
 
 
-def display_result(access, username):
-    if access:
-        print(f'✅ Access granted. Welcome, {username}')
-    else:
-        print('❌ Access denied.')
+def display_result(users):
 
+    attempts = 3
+    while attempts > 0:
+        access = check_credentials()
+
+        if access:
+            print(f'✅ Access granted. Welcome, {users}')
+            break
+        else:
+            print('❌ Access denied.')
+            attempts -= 1
+            print(f'{attempts} attempts left.')
 
 def main():
-    user, password = get_login_input()
-    display_result(check_credentials(user, password), user)
-
-
+    display_result(check_credentials())
 
 main()
