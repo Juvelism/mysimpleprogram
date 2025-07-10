72388517e01e912f570c60e7dcad8303e388656c
Juvs <jborrel01@gmail.com>
2025-07-10 23:13:32 +0800

    updates

--- test_code.py
+++ test_code.py
@@ -1,9 +1,23 @@
 import random
+
 word_list = ["aardvark", "baboon", "camel"]
 
+# Print chosen word
 chosen_word = random.choice(word_list)
 print(chosen_word)
 
+# Print underscore
 placeholder = "_" * len(chosen_word)
 print(placeholder)
+
+guess = input("Guess a word: ")
+display = ""
+
+for letters in chosen_word:
+    if guess == letters:
+        display += guess
+    else:
+        display += "_"
+
+print(display)
 
