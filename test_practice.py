55c12a817bf4137ee1b7623ae928ed4893492a6b
Juvs <jborrel01@gmail.com>
2025-07-11 13:50:09 +0800

    updates

--- test_practice.py
+++ test_practice.py
@@ -1,2 +1,13 @@
 import random
 
+random_words = ["table", "chair", "longboard"]
+
+holder = ""
+get_words = random.choice(random_words)
+print(get_words)
+for letters in range(len(get_words)):
+    holder += "_"
+print(holder)
+
+################################################
+
