5d7b3da09f02e431a57b41b6974c78b69f0256b6
Juvs <jborrel01@gmail.com>
2025-07-12 21:05:57 +0800

    updates

--- test_code.py
+++ test_code.py
@@ -1,29 +1,46 @@
 import random
 
+# 1: GENERATE RANDOM WORDS
 word_list = ["aardvark", "baboon", "camel"]
 random_words = random.choice(word_list)
 print(random_words)
 
+# 2: GENERATE AS MANY BLANKS AS LETTERS IN WORD
 place_holder = ""
 for letters in range(len(random_words)):
     place_holder += "_"
 print(place_holder)
 
+# 3
 placeholder_display = []
+lives = 5
 
-while True:
+while lives > 0:
     guess = input("Guess a letter: ")
 
     if guess in placeholder_display:
         print("You already guessed.")
-    else:
-        placeholder_display.append(guess)
+        continue
+
+    placeholder_display.append(guess)
 
     display = ""
 
+    if guess not in random_words:
+        lives -= 1
+
     for letter in random_words:
-        if letter == guess:
-            display += guess
+        if letter in placeholder_display:
+            display += letter
         else:
             display += "_"
+
+    if "_" not in display:
+        print(random_words)
+        print("You won!")
+        break
+
     print(display)
+    print(f'Your lives: {lives}')
+else:
+    print("You lose!")
