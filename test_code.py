d6315a73ee338d79d48374edd337da2ebf62a4a0
Juvs <jborrel01@gmail.com>
2025-07-13 14:56:02 +0800

    updates

--- test_code.py
+++ test_code.py
@@ -1,46 +1,63 @@
-import random
+stages = [r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
 
-# 1: GENERATE RANDOM WORDS
-word_list = ["aardvark", "baboon", "camel"]
-random_words = random.choice(word_list)
-print(random_words)
-
-# 2: GENERATE AS MANY BLANKS AS LETTERS IN WORD
-place_holder = ""
-for letters in range(len(random_words)):
-    place_holder += "_"
-print(place_holder)
-
-# 3
-placeholder_display = []
-lives = 5
-
-while lives > 0:
-    guess = input("Guess a letter: ")
-
-    if guess in placeholder_display:
-        print("You already guessed.")
-        continue
-
-    placeholder_display.append(guess)
-
-    display = ""
-
-    if guess not in random_words:
-        lives -= 1
-
-    for letter in random_words:
-        if letter in placeholder_display:
-            display += letter
-        else:
-            display += "_"
-
-    if "_" not in display:
-        print(random_words)
-        print("You won!")
-        break
-
-    print(display)
-    print(f'Your lives: {lives}')
-else:
-    print("You lose!")
+for item in reversed(range(len(stages))):
+    user_input = input("Guess a letter: ")
+    if user_input == "1":
+        print("Correct!")
+    else:
+        print(stages[item])
