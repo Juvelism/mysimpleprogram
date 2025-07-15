5050c6961b397dca186222309de5f3fd838dde48
Juvs <jborrel01@gmail.com>
2025-07-15 14:09:24 +0800

    updates

--- another_test.py
+++ another_test.py
@@ -1,9 +1,9 @@
 import random
-words = ["abdomen", "baywatch", "iglob"]
+random_words = ["abdomen", "baywatch", "iglob"]
 
 # Generate random word
 world_holder = ""
-display_word = random.choice(words)
+display_word = random.choice(random_words)
 world_holder += display_word
 print(world_holder)
 
@@ -12,17 +12,26 @@ for letters in range(len(display_word)):
     underscore_display += "_"
 print(underscore_display)
 
+is_game_over = False
+temporary_display = []
 
-is_game_over = True
-
-while is_game_over:
+while not is_game_over:
     guess = input("Guess a letter: ").lower()
+
     display = ""
 
-    for letter in display_word:
+    if guess in temporary_display:
+        print("You already guessed")
+        continue
+
+    temporary_display.append(guess)
+
+    for letter in display:
         if letter == guess:
-            display += letter
+            display += guess
         else:
             display += "_"
 
+
+    print(temporary_display)
     print(display)
