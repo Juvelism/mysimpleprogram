1ee68ae08ab8bda54f68e3899fd2cf5a94899285
Juvs <jborrel01@gmail.com>
2025-07-21 11:21:57 +0800

    updates

--- test_practice.py
+++ test_practice.py
@@ -1,15 +1,12 @@
-alphabet = ['a','b','c','d', 'e', 'f', 'g', 'h', 'i', 'j','k','l', 'm',
-            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+def calculate_love_score(name1, name2):
+    letter_count = {"T": 0, "R": 0, "U": 0, "E": 0}
 
-words = input("Enter the word: ")
-shift_count = int(input("Enter number: "))
 
-index = len(words)
-index_count = alphabet[shift_count]
 
-print(f"word: {words}")
-print(f"len of word: {index}")
-print(f"shift count: {index_count}")
-print(f"{alphabet[index]}")
 
-print(alphabet[index:shift_count+index])
+
+
+
+
+calculate_love_score("Kanye West", "Kim Kardashian")
+
