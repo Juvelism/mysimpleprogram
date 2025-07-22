37fec48d2208150bc0fcf0c67c20ce650513b7d5
Juvs <jborrel01@gmail.com>
2025-07-22 13:36:28 +0800

    updates

--- test_practice.py
+++ test_practice.py
@@ -1,11 +1,15 @@
 def calculate_love_score(name1, name2):
     word = "true"
+    checked = []
+    total_count = 0
 
     for letter in word:
-        if letter in name1:
-            print(letter)
-
-
+        if letter not in checked:
+            count = name1.count(letter)
+            total_count += count
+            times = "time" if count == 1 else "times"
+            print(f"{letter.upper()} occurs {count} {times}")
+    print(f"Total = {total_count}")
 
 calculate_love_score("Kanye West", "Kim Kardashian")
 
