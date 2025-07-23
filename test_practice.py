4bebdd36e2ab98fb5144432ed47f90e354310a0e
Juvs <jborrel01@gmail.com>
2025-07-23 16:08:31 +0800

    updates

--- test_practice.py
+++ test_practice.py
@@ -1,5 +1,32 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
+
+
+# TODO-1: Create a function called 'encrypt()' that takes 'original_text' and 'shift_amount' as 2 inputs.
+def encrypt(original_text, shift_amount):
+
+# TODO-2: Inside the 'encrypt()' function, shift each letter of the 'original_text' forwards in the alphabet
+#  by the shift amount and print the encrypted text.
+    for letter in original_text:
+        message = alphabet.index(letter)
+        print(message)
+
+
+
+# TODO-4: What happens if you try to shift z forwards by 9? Can you fix the code?
+
+# TODO-3: Call the 'encrypt()' function and pass in the user inputs. You should be able to test the code and encrypt a
+#  message.
+
+encrypt(text,shift)
+
+# Let's breakdown the problem:
+# You need a for loop to loop through each of the letters in the plain_text.
+# Find the position of each letter in the alphabet List
+# Add the user desired shift_amount to the position to get the position of the encoded letter.
+# Find the corresponding letter for that position.
+# Add each encoded letter to a new string and print it out after the loop ends.
