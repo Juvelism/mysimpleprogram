27afaf3bc639dc04ddbd238619ad63a95fc67c09
Juvelism <jborrel01@gmail.com>
2025-07-24 16:17:13 +0800

    updats

---
+++ basic_quiz_app/main.py
@@ -0,0 +1,30 @@
+question_and_answer = {
+    {"Question": "What is the capital of Italy?",
+     "Choices": "A. Paris \nB. Rome \nC. Berlin \nD. Madrid",
+     "Answer": "B"
+     },
+
+    {"Question": "What is 5 + 3?",
+    "Choices": "A. 6 \nB. 7 \nC. 8 \nD. 9",
+    "Answer": "C"
+     },
+
+    {"Question": "What color are bananas?",
+    "Choices": "A. Red B. Blue C. Yellow D. Green",
+    "Answer": "C"
+    }
+}
+
+choices = {
+    "A. Paris B. Rome C. Berlin D. Madrid",
+
+
+}
+
+
+score = 0
+count = 0
+
+
+
+
